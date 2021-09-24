6b81cb37c34b9260cfa35addbff0fa48c42bb853
MubashirWaheed <mubashir3080@gmail.com>
2021-09-25 00:29:37 +0500

    Collecting data from implemented

--- src/addproject.js
+++ src/addproject.js
@@ -1,18 +1,38 @@
+import { Project } from "./projectClass";
+import { addNamesTosidebar } from "./sidebarDom";
+
 const addProject = document.querySelector(".add-project");
 const overlay = document.querySelector(".overlay");
 const closeForm = document.querySelector(".close-form");
+const formBtn = document.querySelector(".form-btn");
+const formInput = document.querySelector(".form-input");
+const textArea = document.getElementById("form-text");
+
+let objectsArray = [];
 
 // DISPLAYS FORM
 addProject.addEventListener("click",(e)=>{
     overlay.style.display = "flex";
-})
+    // searchKeyPress();
+});
 
 // CLOSES FORM 
 closeForm.addEventListener("click",(e)=>{
-    console.log(closeForm);
+    // console.log(closeForm);
+    overlay.style.display = "none";
+});
+
+// Collection form data 
+formBtn.addEventListener("click",()=>{
+    let inputValue = formInput.value;
+    let textAreaValue = textArea.value;
+    let arrayItem = new Project(inputValue, textAreaValue);
+    objectsArray.push(arrayItem);
+    console.log(objectsArray);
+    addNamesTosidebar(inputValue);
+    formInput.value = ""; 
+    textArea.value = "";
     overlay.style.display = "none";
 })
 
-
-
-export {addProject};
+export {addProject, objectsArray};
